404aa331de9d9c9b5f9fddabf032a9d501ea1ce3
lisz1012 <lisz1012@163.com>
2020-05-28 01:52:24 -0700

    Assign unique instance-id to the instances behind Ribbon

--- src/main/java/com/lisz/SMSServiceApplication.java
+++ src/main/java/com/lisz/SMSServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+// 可设置启动参数：--server.port=9002
 @SpringBootApplication
 @EnableEurekaClient
 public class SMSServiceApplication {
